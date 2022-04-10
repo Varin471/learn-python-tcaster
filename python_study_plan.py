11199a8fd906a489b19d66e1aa57e657685acdcf
Student <94356640+Varin-js@users.noreply.github.com>
2022-04-10 21:33:32 +0700

    Add files via upload

---
+++ python_study_plan.py
@@ -0,0 +1,19 @@
+# จงเขียนโปรแกรมบวกเลขสองตัว
+
+# สร้างตัวแปร datatype number
+
+a = 1;
+b = 2;
+
+print(a+b); # 3
+
+# -----------------------------
+
+a = int(input());
+print(type(a));
+b = int(input());
+print(type(b));
+
+sum =a+b;
+
+print(sum);
