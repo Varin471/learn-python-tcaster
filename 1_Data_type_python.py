c76f9ec7a9b31ed112b0e14f0d623126f9d77032
Varin Saipanya <94356640+Varin471@users.noreply.github.com>
2022-05-12 22:54:00 +0700

    Add files via upload

---
+++ 1_Data_type_python.py
@@ -0,0 +1,28 @@
+#  ชนิดของข้อมูลในภาษา python
+
+'''
+ตัวเลข (Numbers)
+สายอักขระ (String)
+ลิสต์ (List)
+ทูเพิล (Tuple)
+ดิกชันนารี (Dictionary
+เซต (Set)
+
+อ้างอิงจาก >>> https://ajpaeng.wordpress.com/2020/01/24/datatypes-in-python
+'''
+
+# ชนิดข้อมูลที่เห็นบ่อย
+Str = "ตัวอักษรข้อความ" # string
+Num = 1 # number แบ่งได้ 2 ประเภท คือ 
+Num = 10 # integer จำนวนเต็ม  ||  หรือ จำนวนเต็มลบ ก็ได้ -1 , -2 , -421, ... 
+Num = 834.34 # float จำนวนทศนิยม 
+Bool = True # Boolean ใน python  ค่า True และ False จะต้องขึ้นด้วยตวพิมใหญ่เสมอ
+
+# การ check datatype ของข้อมูลด้วย type()
+print(type(Str)) # <class 'str'>
+print(type(20)) # <class 'int'>
+print(type(Num)) # <class 'float'>
+print(type(Bool)) # <class 'bool'>
+
+# SOURCE: https://github.com/MarbleCode/Intro_python/blob/main/1_Data_type_python_by_Haru.ipynb
+### ==>>>  ผู้สอน haru_tutor   
