606c5ca69d35ae74471f06a04389051b336493e1
Varin Saipanya <94356640+Varin471@users.noreply.github.com>
2022-05-12 22:57:56 +0700

    Update python_study_plan.py

--- python_study_plan.py
+++ python_study_plan.py
@@ -11,9 +11,14 @@ print(a+b); # 3
 
 a = int(input());
 print(type(a));
+
 b = int(input());
 print(type(b));
 
 sum =a+b;
 
 print(sum);
+
+
+# SOURCE: https://github.com/MarbleCode/Intro_python/blob/main/python_study_plan.ipynb
+### ==>>>  ผู้สอน haru_tutor   
