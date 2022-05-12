b991eba6b8497a80017c52a5fb705bcca7193a5f
Varin Saipanya <94356640+Varin471@users.noreply.github.com>
2022-05-12 23:14:16 +0700

    Update numpy_python.py

--- numpy_python.py
+++ numpy_python.py
@@ -49,4 +49,7 @@ import numpy # Numerical Python
 
 # SOURCE: https://github.com/MarbleCode/Intro_python/blob/main/numpy_python_by_Haru.ipynb
 ### ==>>>  ผู้สอน haru_tutor   
+  # instagram: https://www.instagram.com/haru_tutor
+  # youtube: https://www.youtube.com/channel/UCKh8bt1fUttU7d-0J5BnxPg/featured
+  # live: https://www.twitch.tv/haru_marblecode
 
